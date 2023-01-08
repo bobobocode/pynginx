51662564df163167be6a1b3410874bf2941ac99b
BoBoBo <bobobomail@yeah.net>
2023-01-08 13:03:47 +0800

    Save

---
+++ README.md
@@ -0,0 +1,7 @@
+# PyNginx
+
+PyNginx is an Nginx of python version. 
+More than that, it is also an [Engine](https://github.com/bobobocode/engine.git) of python version.
+
+You can use it as same as nginx.
+But now only the features which are showing in the conf directory is supported.
--- pynginx/core/core.py
+++ pynginx/core/core.py
@@ -2,7 +2,7 @@
 
 # BoBoBo
 
-ngx_core_module = {
+ngx_core_module = [
     NGX_MODULE_V1,
     &ngx_core_module_ctx,                  /* module context */
     ngx_core_commands,                     /* module directives */
@@ -15,20 +15,18 @@ ngx_core_module = {
     NULL,                                  /* exit process */
     NULL,                                  /* exit master */
     NGX_MODULE_V1_PADDING
-};
+    ];
 
 
 
-static ngx_command_t  ngx_core_commands[] = {
-    { ngx_string("daemon"),
-      NGX_MAIN_CONF|NGX_DIRECT_CONF|NGX_CONF_FLAG,
-      ngx_conf_set_flag_slot,
-      0,
-      offsetof(ngx_core_conf_t, daemon),
-      NULL },
-
+ ngx_core_commands[] = [
+             [ ngx_string("daemon"),
+              NGX_MAIN_CONF|NGX_DIRECT_CONF|NGX_CONF_FLAG,
+              ngx_conf_set_flag_slot,
+              0,
+              offsetof(ngx_core_conf_t, daemon),
+              NULL ],
 
       ngx_null_command
-};
-
+      ]
 
